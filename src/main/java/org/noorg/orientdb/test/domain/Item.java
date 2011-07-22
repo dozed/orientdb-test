863566f1754eada46bcae8d258557c577c3a2358
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2011-07-22 14:43:40 +0200

    Added id and version to item.

--- src/main/java/org/noorg/orientdb/test/domain/Item.java
+++ src/main/java/org/noorg/orientdb/test/domain/Item.java
@@ -1,6 +1,15 @@
 package org.noorg.orientdb.test.domain;
 
+import javax.persistence.Id;
+import javax.persistence.Version;
+
 public class Item {
+
+	@Id
+	private Long id;
+	
+	@Version
+	private Long version;
 
 	private String title;
 
@@ -12,6 +21,14 @@ public class Item {
 
 	public String getTitle() {
 		return title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getVersion() {
+		return version;
 	}
 
 }
