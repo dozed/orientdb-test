956097968a2119a0e3c14f065803a808534971b8
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2011-08-02 01:15:23 +0200

    Modified save sequence of entities.

--- src/test/java/org/noorg/orientdb/test/ObjectGraphDatabaseTest2.java
+++ src/test/java/org/noorg/orientdb/test/ObjectGraphDatabaseTest2.java
@@ -9,7 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.noorg.orientdb.test.domain.Page;
 
-public class ObjectGraphDatabaseTest {
+public class ObjectGraphDatabaseTest2 {
 
 	@BeforeClass
 	public static void setup() throws IOException {
@@ -19,7 +19,7 @@ public class ObjectGraphDatabaseTest {
 		
 		Page root = new Page("root");
 		repo.save(root);
-
+		
 		root.addPage(new Page("foo"));
 		root.addPage(new Page("bar"));
 
@@ -31,39 +31,10 @@ public class ObjectGraphDatabaseTest {
 	}
 	
 	@Test
-	public void test() {
-		Repository<Page> repo = Repository.get(Page.class);
-		Page root = repo.find("title", "root");
-		goDown(root, 0);
-	}
-	
-	@Test
 	public void testFindAll() {
 		Repository<Page> repo = Repository.get(Page.class);
 		List<Page> pages = repo.findAll();
 		Assert.assertEquals(pages.size(), 5);
 	}
-
-	@Test
-	public void testFind() {
-		Repository<Page> repo = Repository.get(Page.class);
-		Page baz = repo.find("title", "baz");
-		baz.addPage(new Page("nested2"));
-		repo.save(baz);
-
-		Page nested = repo.find("title", "nested2");
-		Assert.assertNotNull(nested);
-	}
-
-	private void goDown(Page page, int level) {
-		for (int i = 0; i < level; i++ ) {
-			System.out.print("--");
-		}
-		System.out.println(page.getTitle());
-		for (Page p : page.getSubPages()) {
-			goDown(p, level+1);
-		}
-	}
-
 
 }
